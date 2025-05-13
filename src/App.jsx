9241219528a3b1ab8b18bb40a5745952a8a249ca
kaitlynmockett <kaitlynm98760@gmail.com>
2025-05-13 15:53:09 +0100

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold text-blue-600">Job Tracker Starter</h1>
+      <p className="text-lg mt-4 text-gray-700">Tailwind + React is working!</p>
+    </div>
+  );
+}
+export default App;
